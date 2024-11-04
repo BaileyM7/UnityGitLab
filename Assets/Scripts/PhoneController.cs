5c95813164b9dac5e3a5d2920e1d28b2048d6b69
Bailey Malota <bmalota08@gmail.com>
2024-11-03 22:09:11 -0500

    adding phone

---
+++ Assets/Scripts/PhoneController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using TMPro; 
+
+public class PhoneController : MonoBehaviour
+{
+    public AudioSource phoneRinging;
+    public TMP_Text displayText;
+    private bool isRinging = false;
+    private string[] words = { "cheese", "pepperoni" }; // possible pizza selection
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(PlayRinging), 60f, 60f); // ring every 60 seconds
+    }
+
+    private void PlayRinging()
+    {
+        if (!isRinging)
+        {
+            phoneRinging.Play();
+            isRinging = true;
+            ClearAndDisplayRandomWord(); // clear textbox and display a random word on each ring
+        }
+    }
+
+    private void ClearAndDisplayRandomWord()
+    {
+        displayText.text = ""; 
+        string selectedWord = words[Random.Range(0, words.Length)]; // select cheese or pepperoni 
+        displayText.text = selectedWord;
+    }
+
+    public void OnPhoneClicked()
+    {
+        if (isRinging)
+        {
+            phoneRinging.Stop();
+            isRinging = false;
+            displayText.text = "";
+        }
+    }
+}
